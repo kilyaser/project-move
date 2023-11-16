02505609cc29520d2a7330932a5f668e2fc01c24
arcadag <piknik0711@yandex.ru>
2023-11-16 21:28:59 +0300

    hw

--- js/script.js
+++ js/script.js
@@ -24,3 +24,30 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    moveList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+moveList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    moveList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+    <div class="delete"></div>
+</li>
+    `;
+});
+
+
+
